11dfd3e9f2a76afce6af11170f83833399ae91e7
Andrey <ninzyago@gmail.com>
2026-07-28 13:35:45 +0300

    feat: lsp for lua

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -62,5 +62,22 @@ return {
         },
       },
     })
+    
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    })
   end,
 }
