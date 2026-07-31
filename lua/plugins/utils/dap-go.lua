59d0007445cb3e8fd63b0e7c5e05c7202a9d5e67
Andrey <ninzyago@gmail.com>
2026-07-31 12:04:28 +0300

    refactor: functions moved to shortcut definition

--- lua/plugins/utils/dap-go.lua
+++ lua/plugins/utils/dap-go.lua
@@ -1,36 +1,3 @@
-local function run_all_go_tests()
-  local file_dir = vim.fn.expand('%:p:h')
-  local file_name = vim.fn.expand('%:t')
-  print("Running all tests in: " .. file_dir .. "/" .. file_name)
-
-  vim.cmd("split | terminal go test -C " .. vim.fn.fnameescape(file_dir) .. " -v")
-  vim.cmd("startinsert")
-end
-
-local function run_closest_go_test()
-  local current_line = vim.fn.line(".")
-  local test_name = nil
-
-  for i = current_line, 1, -1 do 
-    local line = vim.fn.getline(i)
-    test_name = string.match(line, "func%s+(Test[%w_]+)%s*%(")
-    if test_name then
-      break
-    end
-  end
-
-  local file_dir = vim.fn.expand('%:p:h')
-  print("Running test in: " .. file_dir)
-
-  if test_name then
-    vim.cmd("split | terminal go test -C " .. vim.fn.fnameescape(file_dir) .. " -run ^" .. test_name .. "$ -v")
-    vim.cmd("startinsert")
-  else
-    print("Not found any tests above cursor!")
-  end
-end
-
-
 return {
   "leoluz/nvim-dap-go",
   ft = "go",
@@ -59,8 +26,38 @@ return {
       },
     })
 
-    vim.keymap.set("n", "<leader>ra", run_all_go_tests, { desc = "Go: Run all tests in current file" }) 
-    vim.keymap.set("n", "<leader>rt", run_closest_go_test, { desc = "Go: Run nearest go test" })
+    vim.keymap.set("n", "<leader>ra", function ()
+      local file_dir = vim.fn.expand('%:p:h')
+      local file_name = vim.fn.expand('%:t')
+      print("Running all tests in: " .. file_dir .. "/" .. file_name)
+
+      vim.cmd("split | terminal go test -C " .. vim.fn.fnameescape(file_dir) .. " -v")
+      vim.cmd("startinsert")
+    end, { desc = "Go: Run all tests in current file" }) 
+
+    vim.keymap.set("n", "<leader>rt", function()
+      local current_line = vim.fn.line(".")
+      local test_name = nil
+
+      for i = current_line, 1, -1 do 
+        local line = vim.fn.getline(i)
+        test_name = string.match(line, "func%s+(Test[%w_]+)%s*%(")
+        if test_name then
+          break
+        end
+      end
+
+      local file_dir = vim.fn.expand('%:p:h')
+      print("Running test in: " .. file_dir)
+
+      if test_name then
+        vim.cmd("split | terminal go test -C " .. vim.fn.fnameescape(file_dir) .. " -run ^" .. test_name .. "$ -v")
+        vim.cmd("startinsert")
+      else
+        print("Not found any tests above cursor!")
+      end
+    end, { desc = "Go: Run nearest go test" })
+
     vim.keymap.set("n", "<leader>dt", function()
       local original_cwd = vim.fn.getcwd()
 
